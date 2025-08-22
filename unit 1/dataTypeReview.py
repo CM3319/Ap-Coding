73113be4d55eaa204f87b62f4dc18f32bb7f4b0e
CM3319 <cmartin319@boyslatin.net>
2025-08-22 15:09:11 +0000

    got sum stuff down

---
+++ unit 1/dataTypeReview.py
@@ -0,0 +1,25 @@
+print('Christopher Martin')
+
+# print each of the following data types: 
+# string
+# boolean 
+# integer
+# float
+
+
+#2.
+# create and print a list that features 2 of each data type
+a = "Im going to jump off a building"
+
+pritn(
+    a)
+
+
+
+a = 3 
+b= 5 
+
+if b > a
+   print("b is greater than a")
+   else:
+    print("b is not greater than a") 
