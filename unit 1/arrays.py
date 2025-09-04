dff06c30f3ca85e346dcc73762a666bc3df9ce34
CM3319 <cmartin319@boyslatin.net>
2025-09-04 15:15:14 +0000

    done

--- unit 1/arrays.py
+++ unit 1/arrays.py
@@ -3,3 +3,11 @@ for x in numberlist:
     if x == 48:
         break  
 
+unorderlist = [23, 600, 4, 91, 22, 49]
+
+unorderlist.sort(reverse=True)
+print(unorderlist)
+
+
+def bubbleSort(numbers):
+    unorderlist = []
