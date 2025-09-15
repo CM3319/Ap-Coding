842ffc1cea4078937a5709e606d094f7702b6c5b
CM3319 <cmartin319@boyslatin.net>
2025-09-15 15:10:13 +0000

    done

---
+++ unit 1/numberSearch.py
@@ -0,0 +1,14 @@
+
+def minMax():
+    x = min(numbers)
+    y = max(numbers)
+
+    print(x , y)
+
+    
+    minMax()
+fruits = ["apple", "banana", "cherry"]
+fruits.reverse()
+print(fruits) # Output: ['cherry', 'banana', 'apple']
+
+
---
+++ unit 1/objects.py
@@ -0,0 +1,19 @@
+# object is a concept restoring data
+
+class instaProfile:
+    def __init_(self, username, email, profileImg):
+        self.username = username
+        self.email = email 
+        self.profileImg = profileImg
+        self.pw = pw
+
+        def resetPw():
+            print('two step auth...')
+
+            def uploadPicture():
+                print('Instructions')
+
+        def viewFollowers():
+            print('list of other followers')
+
+   profile = instaProfile('Chris, C')
