ec3c702f856c4050ee22372e46c990f0fa171e79
CM3319 <cmartin319@boyslatin.net>
2025-09-17 15:16:24 +0000

    done

--- unit 1/objects.py
+++ unit 1/objects.py
@@ -1,4 +1,4 @@
-# object is a concept restoring data
+# object is a construct for restoring data
 
 class instaProfile:
     def __init_(self, username, email, profileImg):
@@ -17,3 +17,78 @@ class instaProfile:
             print('list of other followers')
 
    profile = instaProfile('Chris, C')
+
+# when creating object we start wwith a classs keyword
+# This acts like our object maker / our keybaord
+
+
+class Carmaker:
+    def __init__(self, color, seating, year, model):
+        self.name = name
+        self.color= color
+        self.setting = setting 
+        self.year = year
+        self.model= model 
+
+        def windshieldwippers():
+            print('when rainin turn on')
+
+        def airbag():
+            print('when drving a certian speed and a collsion happens; open')
+
+        def turnsignal():
+            if up:
+                print("turn left")
+            elif down:
+                print("turn right")
+            else:
+                print("dont turn signals off")
+
+
+        def bluetooth(year):
+             if year > 2020:
+                 print('you have bluetooth')
+            
+
+
+
+
+    def printinfo(self):
+        print('heres your car FAQs')
+        print('name: + self.name')
+        print('year: + str(self.year)')
+        print('Miles:  +str(self.Miles)')
+
+
+
+
+
+def __init__(self.name):
+    self.name = Name
+    # create 3 more attributes
+
+
+def example():
+    # create 3 functions the phne use 
+
+  PhoneA = phoneB()
+
+phoneB = Phone()
+
+
+class iPhone:
+    def __init__(self, color, model, name, size):
+        self.name = name 
+        self.color = color 
+        self.model = model
+        self.size = size 
+    
+    def PhoneStorage():
+        print('Phone has 512GB')
+    def IphoneRam():
+        print('iPhone has 8GB of RAM')
+    def IphoneRing():
+        if Pressed:
+            print("answer call ")
+        if NoAnswer:
+            print("call decline")
