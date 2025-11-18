e1f1699a2bc809f343ac0e8b701f39f6b33feec1
CM3319 <cmartin319@boyslatin.net>
2025-11-18 16:07:39 +0000

    done

---
+++ unit 2/notes.py
@@ -0,0 +1,9 @@
+# clarity - question is easy to understand
+# focused - the question is direct and not too broad
+# measureable - question canbe answered 
+# insightful - provide new information 
+# vague - dont ask questions that arent clear 
+# unmeasureable - dont ask questions that cant be measured 
+# recognize incomplete sentences - dont mistake a cause with a pattern without
+# suffcient data to back it up 
+# complexity - dont make your questions to complex 
---
+++ unit 3/researchQuestion.py
@@ -0,0 +1,28 @@
+from helperLogic import get_team_records, get_advanced_team_records, get_position_columns, get_season_Results_By_team , weeklyPlayerStats, get_advanced_team_records, get_player_stats_by_name, plot_player_stat
+import matplotlib.pyplot as plt
+
+# Columns available to research based on year and position
+# columnData = get_position_columns(2024, "QB")
+
+# '1. How much does QB pass accuracy influence team wins ? '
+# teamRecord = get_team_records(2024)
+#print(teamRecord)
+
+# advanceStats =  get_advanced_team_records(2024)
+# print(advanceStats)
+
+# qbData = weeklyPlayerStats(2024, 'QB')
+#print(qbData)
+
+'2. Does defensive turnovers contribute to a teams win percentage ? '
+# teamStats = get_advanced_team_records(2024)
+# print(teamStats)
+
+# it does because it can disrupt the players opportunites,
+# or being able to score then throwintg a pick and then other 
+# team scores and gets a lead 
+
+'3. Who has the most passing yards of all time ? '
+# the question is to specific because the passing yards 
+# are easily to be containable and the data 
+# has a certian year limit 
