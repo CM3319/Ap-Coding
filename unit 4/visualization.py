e3cc02f5d41adf916a087c64c06a8346ae9a2198
CM3319 <cmartin319@boyslatin.net>
2025-12-04 16:00:00 +0000

    done

--- unit 4/visualization.py
+++ unit 4/visualization.py
@@ -1,8 +1,8 @@
-from helperfunctions import plot_position_stat_bar, export_player_season_png
+from helperfunctions import plot_position_stat_bar, export_player_season_png, plot_player_stat_by_week
 # plot_position_stat_bar(2024,'RB', stat_col = 'rushing_yards', top_n=20, save_path="RB_rushing_yards_2022_2023_2024.png" )
 
 # 2022 was josh jacobbs , 2023 was Christian McCafferey, 2024 was saqoun barkely
 
-export_player_season_png(2022, "Jalen", "Hurts", png_path="Jalen_Hurts_passing_yards_2022")
+plot_player_stat_by_week(2022, "Jalen", "Hurts", stat_col = 'passing_yards', save_path="Jalen_Hurts_passing_yards_2022")
 
-# jalen hurts improved by his passing yards and every other stat increasing every year
+# jalen hurt got worse every year kind've
